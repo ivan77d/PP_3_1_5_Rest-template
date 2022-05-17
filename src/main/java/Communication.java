171d44d246885d95ebc53fa4c563fa8bd2113e32
ivan77d <ivandrokin@gmail.com>
2022-05-17 13:20:09 +0300

    Initial commit

---
+++ src/main/java/Communication.java
@@ -0,0 +1,70 @@
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class Communication {
+    private static final RestTemplate restTemplate = new RestTemplate();
+    private static final String URL = "http://94.198.50.185:7081/api/users";
+
+    public static void main(String[] args) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<List> responseEntity = getAllUsers(requestEntity);
+
+        headers.set("Cookie", responseEntity.getHeaders().get("Set-Cookie")
+                .stream().collect(Collectors.joining(";")));
+
+
+        User user1 = new User();
+        user1.setId(3L);
+        user1.setName("James");
+        user1.setLastName("Brown");
+        user1.setAge((byte) 20);
+
+        HttpEntity<User> entity = new HttpEntity<>(user1, headers);
+        createUser(entity);
+
+        user1.setName("Thomas");
+        user1.setLastName("Shelby");
+        entity = new HttpEntity<>(user1, headers);
+        updateUser(entity);
+
+        deleteUser(entity);
+
+
+    }
+
+    private static void updateUser(HttpEntity<User> entity) {
+        ResponseEntity<String> responseEntity = restTemplate.exchange(URL, HttpMethod.PUT, entity, String.class);
+        System.out.println(responseEntity.getBody());
+        System.out.println("============================");
+
+    }
+
+    private static void deleteUser(HttpEntity<User> entity) {
+        ResponseEntity<String> responseEntity = restTemplate.exchange(URL + "/3", HttpMethod.DELETE, entity, String.class);
+        System.out.println(responseEntity.getBody());
+        System.out.println("============================");
+
+    }
+
+
+    private static void createUser(HttpEntity<User> entity) {
+        ResponseEntity<String> responseEntity = restTemplate.exchange(URL, HttpMethod.POST, entity, String.class);
+        System.out.println(responseEntity.getBody());
+        System.out.println("============================");
+    }
+
+    private static ResponseEntity<List> getAllUsers(HttpEntity<Object> requestEntity) {
+        ResponseEntity<List> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, requestEntity, List.class);
+        System.out.println("============================");
+        return responseEntity;
+    }
+}
